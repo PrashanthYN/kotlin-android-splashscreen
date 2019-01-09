417c40aa83b9ba533d3c831838bef7602a41ebb0
Prashanth YN <ynprashanth2@gmail.com>
2019-01-09 20:55:06 +0530

    comments added

--- app/src/main/java/com/wemaketrends/kotlin_android_splashscreen/SplashActivity.kt
+++ app/src/main/java/com/wemaketrends/kotlin_android_splashscreen/SplashActivity.kt
@@ -10,12 +10,20 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        //create a os handler instance
         val handler=Handler()
+
+        //create a runnable instance
         val runnable= Runnable {
+            //prepare intent to start activity
             val intent=Intent(this,MainActivity::class.java)
+            //start activity
             startActivity(intent)
+            //close present activity
             finish()
         }
+
+        //start thread
         handler.postDelayed(runnable,2000)
     }
 }
